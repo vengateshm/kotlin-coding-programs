68324f297718512c86b567b0671d6ddf41b25f78
vengateshm <40466166+vengateshm@users.noreply.github.com>
2023-10-14 15:09:19 +0530

    round robin addition of fruits into basket

---
+++ src/main/kotlin/collection_programs/FruitsToBasket.kt
@@ -0,0 +1,21 @@
+package collection_programs
+
+fun main() {
+    val fruits = arrayOf("Apple", "Banana", "Orange", "Pineapple", "Pomegranate")
+    val totalBaskets = 3
+    val basketsWithFruits = mutableListOf<MutableList<String>>()
+    repeat(totalBaskets) {
+        basketsWithFruits.add(mutableListOf())
+    }
+
+    var basketIndex = 0
+    for (fruit in fruits) {
+        basketsWithFruits[basketIndex].add(fruit)
+        basketIndex += 1
+        basketIndex %= totalBaskets
+    }
+
+    for (i in 0..<totalBaskets) {
+        println("Basket ${i + 1} : ${basketsWithFruits[i]}")
+    }
+}
