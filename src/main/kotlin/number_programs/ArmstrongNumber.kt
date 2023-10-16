31847a542e49b109ab8dfb7c7d6af7df5ba04ca5
vengateshm <40466166+vengateshm@users.noreply.github.com>
2023-10-16 12:38:42 +0530

    check armstrong number and print within range

--- src/main/kotlin/number_programs/ArmstrongNumber.kt
+++ src/main/kotlin/number_programs/ArmstrongNumber.kt
@@ -1,2 +1,29 @@
 package number_programs
 
+import kotlin.math.pow
+
+// String based length check
+fun isArmstrongNumber(num: Int): Boolean {
+    val length = num.toString().length
+
+    var n = num
+    var newNum = 0
+    while (n != 0) {
+        val rem = n % 10
+        newNum += rem.toDouble().pow(length.toDouble()).toInt()
+        n /= 10
+    }
+
+    return num == newNum
+}
+
+fun printArmstrongNumberForGivenRange(n: Int) {
+    for (i in 1..n) {
+        if (isArmstrongNumber(i)) println(i)
+    }
+}
+
+fun main() {
+    println(isArmstrongNumber(153))
+    println(printArmstrongNumberForGivenRange(10000))
+}
