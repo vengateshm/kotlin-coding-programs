6ecd2914766d785d69b35a8f6d55dbb06a81d3f6
vengateshm <40466166+vengateshm@users.noreply.github.com>
2023-10-16 18:15:49 +0530

    two sum using map

--- src/main/kotlin/array_programs/TwoSum.kt
+++ src/main/kotlin/array_programs/TwoSum.kt
@@ -14,7 +14,23 @@ fun twoSum(arr: IntArray, target: Int): Pair<Int, Int>? {
     return null
 }
 
+// Using map
+fun twoSum1(arr: IntArray, target: Int): Pair<Int, Int>? {
+    val map = HashMap<Int, Int>()
+
+    for (i in arr.indices) {
+        val complement = target - arr[i]
+        if (map.containsKey(complement)) {
+            return Pair(complement, arr[i])
+        }
+    }
+
+    return null
+}
+
 fun main() {
     val result = twoSum(intArrayOf(1, 5, 8, 3), 9)
     println("${result?.first}, ${result?.second}")
+    val result1 = twoSum(intArrayOf(1, 5, 8, 3), 9)
+    println("${result1?.first}, ${result1?.second}")
 }
