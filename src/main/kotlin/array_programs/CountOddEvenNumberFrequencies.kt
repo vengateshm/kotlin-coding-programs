ff245e08e9039984a204fb75a8e2c70573c03ef4
vengateshm <40466166+vengateshm@users.noreply.github.com>
2023-10-16 17:36:17 +0530

    count odd, even number frequencies in int array

--- src/main/kotlin/array_programs/CountOddEvenNumberFrequencies.kt
+++ src/main/kotlin/array_programs/CountOddEvenNumberFrequencies.kt
@@ -1,2 +1,25 @@
 package array_programs
 
+fun countOddEvenNumberFrequencies(arr: IntArray) {
+    var oddCount = 0
+    var evenCount = 0
+    for (num in arr) {
+        if (num % 2 == 0) {
+            evenCount++
+        } else {
+            oddCount++
+        }
+    }
+    println(oddCount)
+    println(evenCount)
+}
+
+fun countOddEvenNumberFrequencies1(arr: IntArray) {
+    println(arr.count { it % 2 != 0 })
+    println(arr.count { it % 2 == 0 })
+}
+
+fun main() {
+    countOddEvenNumberFrequencies(intArrayOf(1, 2, 3, 4, 5, 6))
+    countOddEvenNumberFrequencies1(intArrayOf(1, 2, 3, 4, 5, 6))
+}
