f5e0a3e7267c81c557316448763625720ff456ff
vengateshm <40466166+vengateshm@users.noreply.github.com>
2023-10-16 18:22:30 +0530

    two sum sorted approach

--- src/main/kotlin/array_programs/TwoSum.kt
+++ src/main/kotlin/array_programs/TwoSum.kt
@@ -23,6 +23,28 @@ fun twoSum1(arr: IntArray, target: Int): Pair<Int, Int>? {
         if (map.containsKey(complement)) {
             return Pair(complement, arr[i])
         }
+        map[arr[i]] = arr[i]
+    }
+
+    return null
+}
+
+// Using sorted array approach
+fun twoSum2(arr: IntArray, target: Int): Pair<Int, Int>? {
+    arr.sort()
+    val arrWithIndex = arr.withIndex().toList()
+    val arrWithIndexSorted = arrWithIndex.sortedBy { it.value }
+    var s = 0
+    var e = arrWithIndexSorted.lastIndex
+    while (s < e) {
+        val sum = arrWithIndexSorted[s].value + arrWithIndexSorted[e].value
+        if (sum == target) {
+            return Pair(arrWithIndexSorted[s].value, arrWithIndexSorted[e].value)
+        } else if (sum < target) {
+            s++
+        } else {
+            e--
+        }
     }
 
     return null
@@ -31,6 +53,8 @@ fun twoSum1(arr: IntArray, target: Int): Pair<Int, Int>? {
 fun main() {
     val result = twoSum(intArrayOf(1, 5, 8, 3), 9)
     println("${result?.first}, ${result?.second}")
-    val result1 = twoSum(intArrayOf(1, 5, 8, 3), 9)
+    val result1 = twoSum1(intArrayOf(1, 5, 8, 3), 9)
     println("${result1?.first}, ${result1?.second}")
+    val result2 = twoSum2(intArrayOf(1, 5, 8, 3), 9)
+    println("${result2?.first}, ${result2?.second}")
 }
