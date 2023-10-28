af07fdd348021f8de88e019477d525faacb4177d
vengateshm <40466166+vengateshm@users.noreply.github.com>
2023-10-28 14:02:04 +0530

    sort array list of custom objects

--- src/main/kotlin/collection_programs/SortArrayListCustomObjects.kt
+++ src/main/kotlin/collection_programs/SortArrayListCustomObjects.kt
@@ -4,20 +4,18 @@ data class Employee(val name: String, val age: Int, val salary: Double)
 
 fun main() {
     val employees = listOf(
-        Employee("Alice", 30, 50000.0),
-        Employee("Bob", 25, 60000.0),
-        Employee("Charlie", 35, 55000.0),
-        Employee("David", 25, 20000.0),
-        Employee("Eva", 30, 55000.0)
+        Employee("Alice", 29, 5000.0),
+        Employee("Bob", 25, 6000.0),
+        Employee("Charlie", 35, 5500.0),
+        Employee("David", 25, 2000.0),
+        Employee("Eva", 30, 5500.0)
     )
 
-    // Sort the list of employees first by age in ascending order,
-    // and then by salary in ascending order (secondary sort).
     val sortedEmployees = employees.sortedWith(compareBy<Employee> { it.age }.thenBy { it.salary })
 
     println("Original List of Employees:")
-    employees.forEach { println("Name: ${it.name}, Age: ${it.age}, Salary: ${it.salary}") }
+    employees.forEach { println("Age: ${it.age}, Salary: ${it.salary}, Name: ${it.name}") }
 
     println("\nSorted List of Employees by Age and Salary:")
-    sortedEmployees.forEach { println("Name: ${it.name}, Age: ${it.age}, Salary: ${it.salary}") }
+    sortedEmployees.forEach { println("Age: ${it.age}, Salary: ${it.salary}, Name: ${it.name}") }
 }
